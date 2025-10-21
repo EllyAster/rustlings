b3cf8910edc8f4d2cdbe6344d367698171c729be
Elly Aster <asterelly@gmail.com>
2025-10-21 14:44:36 -0400

    Chapter 6

--- exercises/08_enums/enums2.rs
+++ exercises/08_enums/enums2.rs
@@ -7,6 +7,17 @@ struct Point {
 #[derive(Debug)]
 enum Message {
     // TODO: Define the different variants used below.
+    Resize{width: u8, height: u8},
+    Move (Point),
+    Echo(String),
+    ChangeColor(u8,u8,u8),
+    Quit
+
+}
+
+struct Point{
+    x: u8,
+    y: u8,
 }
 
 impl Message {
