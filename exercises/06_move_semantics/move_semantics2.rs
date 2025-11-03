dfdef2eeaee56fa6bdea45ac1d4f80fb571a33ed
Elly Aster <asterelly@gmail.com>
2025-11-03 12:08:09 -0500

    Ownerhship 1 & 2

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -1,9 +1,9 @@
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec: &Vec<i32>) -> &Vec<i32> {
+    let vecs: &mut Vec<i32> = vec;
 
-    vec.push(88);
+    vecs.push(88);
 
-    vec
+    vecs
 }
 
 fn main() {
@@ -18,11 +18,11 @@ mod tests {
     // fix the compiler error in the test.
     #[test]
     fn move_semantics2() {
-        let vec0 = vec![22, 44, 66];
+        let mut vec0 = vec![22, 44, 66];
 
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(&vec0);
 
-        assert_eq!(vec0, [22, 44, 66]);
-        assert_eq!(vec1, [22, 44, 66, 88]);
+        assert_eq!(*vec0, [22, 44, 66]);
+        assert_eq!(*vec1, [22, 44, 66, 88]);
     }
 }
